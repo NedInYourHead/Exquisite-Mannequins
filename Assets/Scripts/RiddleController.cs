e3b7c9e5088a612d66498fcabfbb5f5d23e14f9b
NedInYourHead <nedcorney@gmail.com>
2022-10-21 12:45:50 +0100

    reworked my ui code to display text only option

--- Assets/Scripts/RiddleController.cs
+++ Assets/Scripts/RiddleController.cs
@@ -3,36 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class RiddleController : MonoBehaviour
 {
     [SerializeField] private GameObject darkScreen;
+    [SerializeField] private GameObject[] textBoxes;
     [SerializeField] private GameObject[] riddleBoxes;
     [SerializeField] private GameObject[] answerBoxes;
-    private int openRiddleIndex;
+    private int openUIIndex;
 
 
     private void Awake()
     {
         for (int i = 0; i < riddleBoxes.Length; i++)
         {
-            openRiddleIndex = i;
+            openUIIndex = i;
+            CloseRiddle();
+        }
+        for (int i = riddleBoxes.Length; i < riddleBoxes.Length + textBoxes.Length; i++)
+        {
+            openUIIndex = i;
             CloseRiddle();
         }
     }
 
     public void OpenRiddle(int riddleIndex)
     {
-        openRiddleIndex = riddleIndex;
+        openUIIndex = riddleIndex;
         darkScreen.SetActive(true);
         riddleBoxes[riddleIndex].SetActive(true);
         answerBoxes[riddleIndex].SetActive(true);
     }
 
+    public void OpenText(int textIndex)
+    {
+        openUIIndex = riddleBoxes.Length + textIndex;
+        darkScreen.SetActive(true);
+        textBoxes[textIndex].SetActive(true);
+    }
+
     public void CloseRiddle()
     {
         darkScreen.SetActive(false);
-        riddleBoxes[openRiddleIndex].SetActive(false);
-        answerBoxes[openRiddleIndex].SetActive(false);
+        if (openUIIndex < riddleBoxes.Length)
+        {
+            riddleBoxes[openUIIndex].SetActive(false);
+            answerBoxes[openUIIndex].SetActive(false);
+        }
+        else
+        {
+            textBoxes[openUIIndex - riddleBoxes.Length].SetActive(false);
+        }
     }
 }
